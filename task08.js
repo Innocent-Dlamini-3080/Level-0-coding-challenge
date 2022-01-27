5d30af3aa8f682acb550fa8eb9f3b897a93ee6b8
Innocent_Dlamini <74040586+Innocent-Dlamini-3080@users.noreply.github.com>
2022-01-27 22:54:54 +0200

    Update task08.js

--- task08.js
+++ task08.js
@@ -4,20 +4,22 @@
 function hoursANdMinutes(number){
     let hours = 0;             
     let minutes = 0;
-    let hourLabel = " hour"
-    minutesLabel = " minutes"
+    let hourLabel = " hour";
+    minutesLabel = " minute";
    
     //caculations of minutes and hours
     while (number > 60){    
-      number-=60;
+      number -= 60;
       hours++;
     }
     minutes = number; 
 
     //add suffixes based on the number
-    if (hours >1  || hours == 0 ){ 
+    if (hours > 1  || hours == 0 ){ 
         hourLabel += "s"
     }
+    if (minutes > 1  || hours == 0 ){ 
+        minutes += "s"
 
     return hours+ hourLabel +", "+ minutes + minutesLabel;
 
